66d4fde7bda653fc0bb5f169151395229af75d51
aliosmandurmaz <odurmaz.34@outlook.com>
2022-04-02 19:20:35 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Ali Osman DURMAZ on 2.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
